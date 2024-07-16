775a3f0febae77d0f2c34b3221616840196a3936
TAsabuki <toshitake.asabuki@gmail.com>
2024-07-16 12:25:22 +0900

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,380 @@
+"""
+Copyright 2022 Toshitake Asabuki
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file recept in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+# coding: UTF-8
+from __future__ import division
+import numpy as np
+import pylab as pl
+import numba
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from tqdm import tqdm
+import seaborn as sns
+from matplotlib import gridspec
+from warnings import simplefilter
+import sklearn.decomposition
+import os
+import shutil
+sns.set(context='paper',style='white',font_scale=1.5,rc={"lines.linewidth":2.5})
+sns.set_palette('muted')
+
+simplefilter(action='ignore', category=FutureWarning)
+mpl.rcParams['svg.fonttype'] = 'none'
+mpl.rcParams['font.sans-serif'] = 'Arial'
+mpl.rcParams['pdf.fonttype'] = 42
+params = {'backend': 'ps',
+          'axes.labelsize': 11,
+          'text.fontsize': 11,
+          'legend.fontsize': 11,
+          'xtick.labelsize': 11,
+          'ytick.labelsize': 11,
+          'text.usetex': False,
+          'figure.figsize': [10 / 2.54, 6 / 2.54]}
+mpl.rcParams['axes.xmargin'] = 0
+mpl.rcParams['axes.ymargin'] = 0
+
+epochs = 1
+for loop in [0]:
+
+
+    W_rec=np.loadtxt("W_E_0.txt", delimiter=',')
+    W_I=np.loadtxt("W_I_0.txt", delimiter=',')
+
+    #W_rec = np.loadtxt("W_E_original.txt", delimiter=',')
+    #W_I = np.loadtxt("W_I_original.txt", delimiter=',')
+    transition_prob = [[0,0.1,0.9],[0.1,0,0.9],[0.5,0.5,0]]
+    pat_list = [0, 1, 2]
+
+    #transition_prob = [[0,1/2,1/2,0,0],[1/3,0,1/3,1/3,0],[1/3,0,0,1/3,1/3],[0,1/2,1/2,0,0],[0,0,1,0,0]]
+    #pat_list = [0, 1, 2, 3,4]
+    N = len(W_rec[0,:])
+    N_E = int(N * 1 / 2)
+    N_I = N - N_E
+
+    n_pat = len(set(pat_list))
+
+
+    np.random.seed()
+
+    alpha = 1
+    theta = 1
+    beta = 5
+    @numba.njit(fastmath=True, nogil=True)
+    def g(x):
+        ans = 1 / (1 + alpha * np.exp(beta * (-x + theta)))
+        return ans
+
+    vfunc = np.vectorize(g)
+    fig = plt.figure(figsize=(4, 4))
+    ax1 = fig.add_subplot(111)
+    pl.plot(np.arange(-1, 2, 0.01), vfunc(np.arange(-1, 2, 0.01)))
+    fig.subplots_adjust(bottom=0.25, left=0.15)
+    ax1.xaxis.set_ticks_position('bottom')
+    ax1.yaxis.set_ticks_position('left')
+    ax1.spines['right'].set_color('none')
+    ax1.spines['top'].set_color('none')
+    plt.savefig('sigmoid.pdf', format='pdf', dpi=350)
+
+    raeter_size = 1.1
+    dt = 1
+    test_len = int(100 * 1000 / dt)
+    test_len2 = int(100 * 1000 / dt)
+    plot_len = test_len2
+
+    gain = 1
+    width = 200
+    y = np.random.rand(N)
+
+    f = g(y)
+
+    tau_m = 15
+    tau_syn = 5
+
+    n_input = 100*n_pat
+    pat_color = sns.color_palette("Set2")
+    max_rate = 0.05
+
+    I_syn = 0*np.random.rand(N)/tau_m/tau_syn
+    PSP = np.zeros(N)
+
+    tau_max = 10*1000
+    max_trace = np.zeros(N) + 1
+
+    id_rec = np.zeros(N,dtype=bool)
+
+    """
+    Spontaneous
+    """
+
+
+
+    dt = 1
+
+    gain = 1
+
+    width = 200
+
+    y = np.random.rand(N)
+
+    f = g(y)
+
+    tau_m = 15
+    tau_syn = 5
+
+    n_input = 100*n_pat
+
+    p_input = 1
+    p_rec = 1
+
+    I_syn = np.random.rand(N)/tau_m/tau_syn
+    PSP = np.zeros(N)
+
+    id_rec = np.zeros(N, dtype=bool)
+
+    mu = np.zeros(N)
+    mu_square = np.zeros(N) + 1
+
+    gamma_mu = 0.0001
+    gamma_mu_square = gamma_mu
+
+    count = 0
+
+    g_cp = 2
+
+    pat_mat_supervise = np.zeros((N, n_pat))
+    for i in range(n_pat):
+        pat_mat_supervise[int(i*N_E/n_pat):int((i+1)*N_E/n_pat), i] = 0.5
+
+    max_trace = np.zeros(N) + 1
+    min_trace = np.zeros(N) + 10 ** (-5)
+
+    max_trace_exc = np.zeros(N) + 1
+
+    tau_max = 10*1000
+    tau_max_exc = 10*1000
+
+    const_noise = np.zeros(N)
+    const_noise[N_E:] = 0
+    const_noise[0:N_E] = 0.3
+
+
+    """
+    Spont1
+    """
+    pat_start = 0
+    pat_start_list_blank = []
+    for i in range(n_pat):
+        pat_start_list_blank.append([])
+    count = 0
+    pattern_id=0
+
+
+    pat_count = 0
+    for i in tqdm(range(test_len), desc="[spont1]"):
+
+        I_syn = (1.0 - dt / tau_syn) * I_syn
+        I_syn[id_rec]+=1/tau_m/tau_syn
+        PSP = (1.0 - dt / tau_m) * PSP + I_syn*25
+
+        rec_term = np.dot(W_rec, PSP)
+        M_term = np.dot(W_I, PSP)
+        x = rec_term-M_term+ const_noise
+
+        max_trace = (1.0 - dt / tau_max) * max_trace
+        max_trace[max_trace < x * gain]  = x[max_trace < x * gain]
+        max_trace[N_E:] = 2
+
+        y = 2*x / max_trace
+
+        f = g(y)
+
+        id_rec = (np.random.rand(N) < f * dt * max_rate)
+
+    """
+    Spont2
+    """
+
+    x_list = np.zeros((N,test_len2))
+    id_list_E = np.zeros((test_len2, N_E), dtype=bool)
+    f_list = np.zeros((N_E, test_len2))
+
+    filtered_spikes = np.zeros(N_E)
+    filtered_spikes_list = np.zeros((N_E, test_len2))
+    tau_filter = 100
+
+    EPSCs = np.zeros((n_pat,test_len2))
+    IPSCs = np.zeros((n_pat,test_len2))
+
+    for i in tqdm(range(test_len2), desc="[spont2]"):
+
+        I_syn = (1.0 - dt / tau_syn) * I_syn
+        I_syn[id_rec]+=1/tau_m/tau_syn
+        PSP = (1.0 - dt / tau_m) * PSP + I_syn*25
+        rec_term = np.dot(W_rec, PSP)
+        M_term = np.dot(W_I, PSP)
+        x = rec_term-M_term+ const_noise
+
+        max_trace = (1.0 - dt / tau_max) * max_trace
+        max_trace[max_trace < x * gain]  = x[max_trace < x * gain]
+        max_trace[N_E:] = 2
+
+        y = 2*x / max_trace
+
+        f = g(y)
+
+        for mm in range(n_pat):
+            EPSCs[mm,i] = np.mean(np.dot(W_rec[int(mm)*int(N_E/n_pat):int(mm+1)*int(N_E/n_pat),:],PSP))
+        for mm in range(n_pat):
+            IPSCs[mm,i] = np.mean(np.dot(W_I[int(mm)*int(N_E/n_pat):int(mm+1)*int(N_E/n_pat),:],PSP))
+
+        x_list[:,i] =  2 * y + const_noise
+
+        id_rec = (np.random.rand(N) < f * dt * max_rate)
+
+        filtered_spikes = (1-dt/tau_filter)*filtered_spikes + id_rec[0:N_E]
+
+        f_list[:, i] = f[0:N_E]
+        filtered_spikes_list[:,i]=filtered_spikes
+        id_list_E[i, :] = id_rec[0:N_E]
+
+    print(1)
+
+    pl.figure()
+
+    fig = plt.figure(figsize=(4, 4))
+
+    mean_std_vol = np.zeros(4)
+    mean_std_vol[0] = np.mean(np.mean(x_list[0:N_E,:],axis=1))
+    mean_std_vol[1] = np.mean(np.std(x_list[0:N_E,:],axis=1))
+    mean_std_vol[2] = np.mean(np.mean(x_list[N_E:,:],axis=1))
+    mean_std_vol[3] = np.mean(np.std(x_list[N_E:,:],axis=1))
+
+
+    np.savetxt('mean_std_vol.txt', mean_std_vol, delimiter=',')
+
+    mean_firing_rate = np.zeros((n_pat, test_len2))
+    count = 0
+    for i in range(n_pat):
+
+        mean_firing_rate[i, :] = np.mean(f_list[i*int(N_E/n_pat):(i+1)*int(N_E/n_pat), 0:test_len2], axis=0)
+
+    np.savetxt('assembly_acvitities.txt', mean_firing_rate, delimiter=',')
+
+    firing_mat = f_list[0:N_E, 0:2000].T
+
+    pca = sklearn.decomposition.PCA(n_components=2)
+    pca.fit(firing_mat)
+
+    # print(np.sum(pca.explained_variance_ratio_))
+    np.savetxt('variance_explained.txt', pca.explained_variance_ratio_, delimiter=',')
+
+
+
+    mean_firing_rate_filtered_spikes = np.zeros((n_pat,test_len2))
+    count = 0
+    for i in range(n_pat):
+
+        mean_firing_rate_filtered_spikes[i, :] = np.mean(filtered_spikes_list[i*int(N_E/n_pat):(i+1)*int(N_E/n_pat), 0:test_len2], axis=0)
+
+    np.savetxt('assembly_acvitities_filtered_spikes.txt', mean_firing_rate_filtered_spikes, delimiter=',')
+
+    print("raster_start")
+    pl.figure()
+    plot_len2 = 2 * 1000
+    fig = plt.figure(figsize=(5, 3))
+    ax = fig.add_subplot(111)
+    ax.tick_params(direction="in", width=1,length = 2)
+    for nn in range(n_pat):
+        tspk, nspk = pl.nonzero(id_list_E[test_len2 - plot_len2:, nn*int(N_E/n_pat):(nn+1)*int(N_E/n_pat)] == 1)
+        nspk += nn*int(N_E/n_pat)
+        plt.plot(tspk, nspk, c=pat_color[nn],marker='o',lw=0,markersize=0.8,fillstyle='full',markeredgewidth=0.0)
+
+    plt.ylabel("Neurons"+ r"$\ \times \ 10^2$ ", fontsize=10)
+    plt.xlabel("Time (s)", fontsize=10)
+    pl.xlim([0, plot_len2])
+    #plt.xticks([0, 2*1000, 4*1000, 6*1000, 8*1000, 9.9*1000],
+               #['0', '2', '4', '6', '8', '10'], fontsize=10)
+    #plt.yticks([100, 200, 300, 400, 499],
+               #['1', '2', '3', '4', '5'], fontsize=10)
+    # plt.xticks(time_axis,np.arange(second_spont+1),fontsize=11)
+
+    fig.subplots_adjust(bottom=0.25, left=0.15)
+
+    # ax1.xaxis.set_ticks_position('bottom')
+    # ax1.yaxis.set_ticks_position('left')
+    # ax1.spines['right'].set_color('none')#
+    # ax1.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+
+    ax.spines["left"].set_linewidth(1)
+    ax.spines["bottom"].set_linewidth(1)
+
+    plt.savefig('raster_spont.pdf', dpi=350)
+
+    print("raster_finish")
+    #pl.plot()
+
+    np.savetxt('EPSCs.txt', EPSCs, delimiter=',')
+    np.savetxt('IPSCs.txt', IPSCs, delimiter=',')
+
+
+    pl.figure()
+    plot_len2 = 2 * 1000
+    fig = plt.figure(figsize=(4, 3))
+    gs = gridspec.GridSpec(2, 1, height_ratios=[1, 2])
+    ax = plt.subplot(gs[1])#fig.add_subplot(212)
+    ax.tick_params(direction="in", width=1,length = 2)
+    for nn in range(n_pat):
+        tspk, nspk = pl.nonzero(id_list_E[test_len2 - plot_len2:, nn*int(N_E/n_pat):(nn+1)*int(N_E/n_pat)] == 1)
+        nspk += nn*int(N_E/n_pat)
+        plt.plot(tspk, nspk, c=pat_color[nn],marker='o',lw=0,markersize=raeter_size,fillstyle='full',markeredgewidth=0.0)
+
+    plt.ylabel("Neurons"+ r"$\ \times \ 10^2$ ", fontsize=10)
+    plt.xlabel("Time (s)", fontsize=10)
+    pl.xlim([0, plot_len2])
+    #plt.xticks([0, 2*1000, 4*1000, 6*1000, 8*1000, 9.9*1000],
+               #['0', '2', '4', '6', '8', '10'], fontsize=10)
+    #plt.yticks([100, 200, 300, 400, 499],
+               #['1', '2', '3', '4', '5'], fontsize=10)
+    # plt.xticks(time_axis,np.arange(second_spont+1),fontsize=11)
+
+    fig.subplots_adjust(bottom=0.25, left=0.15)
+
+    # ax1.xaxis.set_ticks_position('bottom')
+    # ax1.yaxis.set_ticks_position('left')
+    # ax1.spines['right'].set_color('none')#
+    # ax1.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+
+    ax.spines["left"].set_linewidth(1)
+    ax.spines["bottom"].set_linewidth(1)
+    ax = plt.subplot(gs[0])#fig.add_subplot(211)
+    ax.tick_params(direction="in", width=0,length = 0)
+    for i in range(n_pat):
+        pl.plot(mean_firing_rate[i, test_len2 - plot_len2:], c=pat_color[i],lw = 1.3)
+        # plt.hlines(activation_threshold[i],0,50*10,color=pat_color[i])
+        pl.ylim([0,1])
+    plt.xticks([])
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    plt.savefig('raster_spont.pdf', dpi=350)
---
+++ training.py
@@ -0,0 +1,274 @@
+"""
+Copyright 2022 Toshitake Asabuki
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file recept in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+# coding: UTF-8
+from __future__ import division
+import numpy as np
+import pylab as pl
+import numba
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from tqdm import tqdm
+from warnings import simplefilter
+from numpy import linalg as LA
+import os
+
+simplefilter(action='ignore', category=FutureWarning)
+mpl.rcParams['svg.fonttype'] = 'none'
+mpl.rcParams['font.sans-serif'] = 'Arial'
+mpl.rcParams['pdf.fonttype'] = 42
+params = {'backend': 'ps',
+          'axes.labelsize': 11,
+          'text.fontsize': 11,
+          'legend.fontsize': 11,
+          'xtick.labelsize': 11,
+          'ytick.labelsize': 11,
+          'text.usetex': False,
+          'figure.figsize': [10 / 2.54, 6 / 2.54]}
+
+#transition_prob = [[0,1/2,1/2,0,0],[1/3,0,1/3,1/3,0],[1/3,0,0,1/3,1/3],[0,1/2,1/2,0,0],[0,0,1,0,0]]
+#state_list = [0, 1, 2, 3,4]
+transition_prob = [[0,0.1,0.9],[0.1,0,0.9],[0.5,0.5,0]]
+state_list = [0, 1, 2]
+n_pat = len(set(state_list)) #num of states
+
+N = 1000 #network size
+N_E = int(N * 1 / 2) #num of exc neurons
+N_I = N - N_E #num of inh neurons
+
+## activation function
+alpha = 1
+theta = 1
+beta = 5
+@numba.njit(fastmath=True, nogil=True)
+def g(x):
+    ans = 1 / (1 + alpha * np.exp(beta * (-x + theta)))
+    return ans
+###
+
+## learning rule
+@numba.njit(parallel=True, fastmath=True, nogil=True)
+def learning(w, g_V_star, PSP_star, eps, g_V_som, mask):
+
+    for i in numba.prange(len(w[:, 0])):
+        for l in numba.prange(len(w[0, :])):
+            delta = (-(g_V_star[i]) + g_V_som[i]) * PSP_star[l]
+            w[i, l] += eps * delta
+            w[i, l] *= mask[i, l]
+            w[i, l] *= (w[i, l] >= 0)
+    return w
+##
+
+dt = 1 #integration time step
+
+dur_state = 200 # duration of states in ms
+
+eps = 10 ** (-5) # learning rate
+
+msecs_learning =200 * 1000 # ms for learning
+simtime = np.arange(0, msecs_learning, dt)
+simtime_len = len(simtime)
+
+tau_m = 15 # membrane time constant
+tau_syn = 5 # synaptic time constant
+
+max_rate = 0.05 #maxumus firing rate of sigmoid
+
+p_connect = 0.5 # connection probability
+
+n_sim=1
+state_list = np.zeros((simtime_len,n_sim))
+error_E_square = np.zeros((simtime_len, n_sim))  # error for E plasticity
+error_I_square = np.zeros((simtime_len, n_sim))  # error for I plasticity
+
+error_E_square2 = np.zeros((simtime_len, n_sim))  # error for E plasticity
+
+state3_start_time = []
+
+error_filtered_list_E_mean = np.zeros((n_sim,simtime_len))
+error_filtered_list_E_std = np.zeros((n_sim,simtime_len))
+error_filtered_list_I_mean = np.zeros((n_sim,simtime_len))
+error_filtered_list_I_std = np.zeros((n_sim,simtime_len))
+for sim in range(n_sim):
+
+    E_potential = np.zeros(simtime_len)
+    I_potential = np.zeros(simtime_len)
+    E_potential_sigmoid = np.zeros(simtime_len)
+    I_potential_sigmoid = np.zeros(simtime_len)
+
+    Ext_potential = np.zeros(simtime_len)
+    f_list = np.zeros(simtime_len)
+    x_within_list = np.zeros(simtime_len)
+    x_between_list = np.zeros(simtime_len)
+
+    W_E = np.ones((N, N)) / np.sqrt(p_connect*N_E)*1 # E connections
+    W_E[0:N_E,0:N_E]*=0.1
+    W_E_mask = np.zeros((N, N)) # E connectivity
+    W_E_mask[:, 0:N_E] = (np.random.rand(N,N_E)<p_connect)
+    W_E_mask[np.eye(N, dtype=bool)] = 0
+    W_E *= W_E_mask
+
+    W_I = np.ones((N,N))/np.sqrt(p_connect*N_I)*1 # I connections
+    W_I[0:N_E,N_E:] *= 0.1
+    W_I_mask = np.zeros((N, N)) # I connectivity
+    W_I_mask[:,N_E:] = (np. random.rand(N,N_I)<p_connect)
+    W_I_mask[np.eye(N, dtype=bool)] = 0
+    W_I*=W_I_mask
+
+    I_syn = np.zeros(N) # synaptic current
+    PSP = np.zeros(N) # post synaptic potential
+
+    id_rec = np.zeros(N, dtype=bool) # neurons that spike (at each time)
+
+    strength_input = 2 # strength of external input
+    input_mat = np.zeros((N, n_pat)) # set of external input
+    for i in range(n_pat):
+        input_mat[int(i*N_E/n_pat):int((i+1)*N_E/n_pat), i] = strength_input
+
+    """
+    Training
+    """
+    PSP_amp = (tau_m-tau_syn)/((tau_m/tau_syn)**(-(tau_syn)/(tau_m-tau_syn))-(tau_m/tau_syn)**(-(tau_m)/(tau_m-tau_syn)))
+
+    pat_start = 0 # time of next stimulus presentation
+    pattern_id = 0 # initial state
+    max_trace = np.zeros(N) + 1 # exponential trace for tracking maximum value of membrane potential
+    tau_max = 10*1000 # time scale of max_trace
+
+    delta_weight_save = 2000 # synaptic weights are saved every 2 sec.
+    W_E_list = np.zeros((N_E,N_E,int(simtime_len/delta_weight_save)))  # array of saved W_E
+    W_I_list = np.zeros((N_E, N_I, int(simtime_len / delta_weight_save))) # array of saved W_I
+
+    W_E_between = np.ones((N,N))
+    W_E_between[:,N_E:] *= 0
+    for i in range(n_pat):
+        W_E_between[int(i*N_E/n_pat):int((i+1)*N_E/n_pat),int(i*N_E/n_pat):int((i+1)*N_E/n_pat)]*=0
+    W_E_within = np.zeros((N,N))
+    W_E_within[:,N_E:] *= 0
+    for i in range(n_pat):
+        W_E_within[int(i*N_E/n_pat):int((i+1)*N_E/n_pat),int(i*N_E/n_pat):int((i+1)*N_E/n_pat)]=1
+
+    error_filtered_E = np.zeros(int(N_E/1))
+    error_filtered_I = np.zeros(int(N_E/1))
+
+
+    error_list = np.zeros(simtime_len)
+    # training
+    for i in tqdm(range(simtime_len), desc="[training]"):
+
+
+
+        if i == pat_start:
+
+            if pattern_id == 2:
+                state3_start_time.append(i)
+
+            I_ext = input_mat[:,pattern_id] # exteternal corrent
+            pat_start += dur_state #time of next stimulus presentation
+            ### updating state
+            next_prob = transition_prob[pattern_id]
+            dice = np.random.rand()
+            for xx in range(n_pat):
+                if dice>np.sum(next_prob[0:xx]):
+                    if dice<=np.sum(next_prob[0:xx+1]):
+                        pattern_id=xx
+                        break
+
+
+            #print(state_list[i + dur_state:i + 2 * dur_state,sim])
+        # updating recurrent synaptic current and PSP
+        I_syn = (1.0 - dt / tau_syn) * I_syn
+        I_syn[id_rec]+=1/tau_m/tau_syn
+        PSP = (1.0 - dt / tau_m) * PSP + I_syn*PSP_amp
+
+        I_term = np.dot(W_I, PSP) # inhibitory effect on membrane potential
+        input_term = I_ext # external input effect on membrane potential
+        E_term = np.dot(W_E, PSP)
+
+        x = input_term + E_term - I_term # total membrane potential
+
+
+
+        # updating max_trace
+        max_trace = (1.0 - dt / tau_max) * max_trace
+        max_trace[max_trace < x]  = x[max_trace < x]
+        max_trace[N_E:] = 2
+
+        y = x / max_trace # normalized membrane potential with max_trace. This will be replaced with homeostatis of weights?
+
+        f = g(2*y) # firing rate of neurons. y is scaled with a constant value of 2.
+
+        if i>0:
+            error_filtered_E = (1.0 - 1 / 30000) * error_filtered_E + ((f[int(0*N_E/n_pat):int((2+1)*N_E/n_pat)] - g(E_term)[int(0*N_E/n_pat):int((2+1)*N_E/n_pat)])) / 30000
+            error_filtered_I = (1.0 - 1 / 30000) * error_filtered_I + ((g(E_term)[int(0 * N_E / n_pat):int((2 + 1) * N_E / n_pat)] - g(I_term)[int(0 * N_E / n_pat):int((2 + 1) * N_E / n_pat)])) / 30000
+        else:
+            error_filtered_E = (f[int(0*N_E/n_pat):int((2+1)*N_E/n_pat)] - g(E_term)[int(0*N_E/n_pat):int((2+1)*N_E/n_pat)])
+            error_filtered_I =(g(E_term)[int(0 * N_E / n_pat):int((2 + 1) * N_E / n_pat)] - g(I_term)[int(0 * N_E / n_pat):int((2 + 1) * N_E / n_pat)])
+        error_filtered_list_E_mean[sim,i] = np.mean(np.abs(error_filtered_E))
+        error_filtered_list_E_std[sim,i] = np.std(np.abs(error_filtered_E))
+
+        error_filtered_list_I_mean[sim,i] = np.mean(np.abs(error_filtered_I))
+        error_filtered_list_I_std[sim,i] = np.std(np.abs(error_filtered_I))
+
+        id_rec = (np.random.rand(N) < f * dt * max_rate) # neuron id that emit spikes
+
+        # training. only synapses onto E-neurons are plastic.
+        W_E[0:N_E,:] = learning(W_E[0:N_E,:], g(E_term)[0:N_E], PSP, eps, f[0:N_E], W_E_mask[0:N_E,:])
+        W_I[0:N_E,:] = learning(W_I[0:N_E,:], g(I_term)[0:N_E], PSP, eps, g(E_term)[0:N_E], W_I_mask[0:N_E,:])
+
+        # saving weights
+        if i%delta_weight_save == 0:
+            W_E_list[:, :, int(i / delta_weight_save)] = W_E[0:N_E, 0:N_E]
+            W_I_list[:, :, int(i / delta_weight_save)] = W_I[0:N_E, N_E:]
+
+
+        # saving errors
+        error_list[i] = np.mean(f-g(E_term))
+
+    # learning curve
+    LC_E = []
+    LC_I = []
+    for kk in range(int(simtime_len/delta_weight_save)):
+        if kk>0:
+            LC_E.append(LA.norm(W_E_list[:, :, kk] - W_E_list[:, :, kk - 1])/(N_E**2))
+            LC_I.append(LA.norm(W_I_list[:, :, kk] - W_I_list[:, :, kk - 1])/(N_E*N_I))
+
+    np.savetxt('learning_curve_E.txt', LC_E, delimiter=',')
+    np.savetxt('learning_curve_I.txt', LC_I, delimiter=',')
+
+    arr_reshaped = W_E_list.reshape(W_E_list.shape[0], -1)
+    # saving reshaped array to file.
+    np.savetxt("W_E_list.txt", arr_reshaped)
+
+    np.savetxt('W_E_%s.txt'%(sim), W_E, delimiter=',')
+    np.savetxt('W_I_%s.txt'%(sim), W_I, delimiter=',')
+
+    plt.close('all')
+
+np.savetxt("error_list.txt", error_list, delimiter=',')
+
+np.savetxt("E_potential.txt", E_potential, delimiter=',')
+np.savetxt('I_potential.txt', I_potential, delimiter=',')
+
+np.savetxt("E_potential_sigmoid.txt", E_potential_sigmoid, delimiter=',')
+np.savetxt('I_potential_sigmoid.txt', I_potential_sigmoid, delimiter=',')
+
+np.savetxt('Ext_potential.txt', Ext_potential, delimiter=',')
+np.savetxt('f_list.txt', f_list, delimiter=',')
+
+np.savetxt('error_filtered_list_E_mean.txt', error_filtered_list_E_mean, delimiter=',')
+np.savetxt('error_filtered_list_E_std.txt', error_filtered_list_E_std, delimiter=',')
+np.savetxt('error_filtered_list_I_mean.txt', error_filtered_list_I_mean, delimiter=',')
+np.savetxt('error_filtered_list_I_std.txt', error_filtered_list_I_std, delimiter=',')
