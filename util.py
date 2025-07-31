4f8437de78bbb564ee946c0c9d447a6776b25c8c
gettodaze <jmccloskey2@wisc.edu>
2025-07-31 20:25:17 +0900

    comment

--- util.py
+++ util.py
@@ -11,13 +11,13 @@ def configure_mpl():
     mpl.rcParams["svg.fonttype"] = "none"
     mpl.rcParams["font.sans-serif"] = "Arial"
     mpl.rcParams["pdf.fonttype"] = 42
-    params = {
-        "backend": "ps",
-        "axes.labelsize": 11,
-        "text.fontsize": 11,
-        "legend.fontsize": 11,
-        "xtick.labelsize": 11,
-        "ytick.labelsize": 11,
-        "text.usetex": False,
-        "figure.figsize": [10 / 2.54, 6 / 2.54],
-    }
+    # params = {
+    #     "backend": "ps",
+    #     "axes.labelsize": 11,
+    #     "text.fontsize": 11,
+    #     "legend.fontsize": 11,
+    #     "xtick.labelsize": 11,
+    #     "ytick.labelsize": 11,
+    #     "text.usetex": False,
+    #     "figure.figsize": [10 / 2.54, 6 / 2.54],
+    # }
